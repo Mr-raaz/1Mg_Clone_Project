c126c5c774da00ad5f1c05c0a655b53902ee01cd
Rajatsinha05 <rajatsinha5467@gmail.com>
2022-09-07 20:17:41 +0530

    product added

--- Landing_page.js
+++ Landing_page.js
@@ -23,3 +23,23 @@ function changeImage(){
 
 window.onload = changeImage;
 // Slider section ends here
+var swiper = new Swiper(".mySwiper", {
+  slidesPerView: 7,
+  spaceBetween: 0,
+ 
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  breakpoints:{
+  0: {
+      slidesPerView: 1,
+  },
+  520: {
+      slidesPerView: 4,
+  },
+  950: {
+      slidesPerView: 7,
+  },
+},
+});
